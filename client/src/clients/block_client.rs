4ebc7c4fd75d2b1a2426836dd4c30873cbae2a77
Marko Petrlić <petrlic.marko@gmail.com>
2025-07-16 21:09:03 +0200

    Updated block_transactions method to be more sane (#39)

--- client/src/clients/block_client.rs
+++ client/src/clients/block_client.rs
@@ -1,7 +1,10 @@
 use super::Client;
 use avail_rust_core::{
-	FetchExtrinsicsV1Options, H256, HashNumber,
-	rpc::{self, system::fetch_extrinsics_v1_types as Types},
+	EncodeSelector, FetchExtrinsicsV1Options, H256, HashNumber,
+	rpc::{
+		self,
+		system::fetch_extrinsics_v1_types::{self as Types, Filter, SignatureFilter, TransactionFilter},
+	},
 };
 
 #[derive(Clone)]
@@ -14,18 +17,24 @@ impl BlockClient {
 		Self { client }
 	}
 
+	/// Fetches specific transaction from a block.
+	/// Transaction can be filtered by signature characteristics
+	///
+	/// By default, the api will request and return just the (Hex and SCALE decoded) Transaction Call
+	/// If the full Transaction is needed or no data is needed then set `encode_as` to either
+	/// `EncodeSelector::Extrinsic` or `EncodeSelector::None`
 	pub async fn block_transaction(
 		&self,
 		block_id: HashNumber,
 		transaction_id: HashNumber,
-		sig_filter: Option<Types::SignatureFilter>,
-		selector: Option<Types::EncodeSelector>,
+		signature_filter: Option<SignatureFilter>,
+		selector: Option<EncodeSelector>,
 	) -> Result<Option<Types::ExtrinsicInformation>, avail_rust_core::Error> {
 		let filter = match transaction_id {
 			HashNumber::Hash(item) => Types::TransactionFilter::TxHash(vec![item]),
 			HashNumber::Number(item) => Types::TransactionFilter::TxIndex(vec![item]),
 		};
-		let filter = Some(Types::Filter::new(Some(filter), sig_filter));
+		let filter = Some(Types::Filter::new(Some(filter), signature_filter));
 		let params = FetchExtrinsicsV1Options::new(filter, selector);
 		let mut result = self
 			.client
@@ -40,17 +49,32 @@ impl BlockClient {
 		Ok(Some(result.remove(0)))
 	}
 
+	/// Fetches transactions from a block.
+	/// Transactions can be filtered by transaction and/or signature characteristics
+	///
+	/// By default, the api will request and return just the (Hex and SCALE decoded) Transaction Call
+	/// If the full Transaction is needed or no data is needed then set `encode_as` to either
+	/// `EncodeSelector::Extrinsic` or `EncodeSelector::None`
 	pub async fn block_transactions(
 		&self,
 		block_id: HashNumber,
-		options: Option<Types::Options>,
+		transaction_filter: Option<TransactionFilter>,
+		signature_filter: Option<SignatureFilter>,
+		encode_as: Option<EncodeSelector>,
 	) -> Result<Types::Output, avail_rust_core::Error> {
+		let filter = Filter::new(transaction_filter, signature_filter);
+		let options = Types::Options::new(Some(filter), encode_as);
+
 		self.client
 			.rpc_api()
-			.system_fetch_extrinsics_v1(block_id, options)
+			.system_fetch_extrinsics_v1(block_id, Some(options))
 			.await
 	}
 
+	/// Fetches a block at a specific block hash
+	/// A block contains a block header, all the transactions and all the justifications
+	///
+	/// In 99.99% cases `.block_transaction` or `.block_transactions` method is the one that you need/want
 	pub async fn rpc_block(&self, at: H256) -> Result<Option<rpc::BlockWithJustifications>, avail_rust_core::Error> {
 		self.client.block(at).await
 	}
